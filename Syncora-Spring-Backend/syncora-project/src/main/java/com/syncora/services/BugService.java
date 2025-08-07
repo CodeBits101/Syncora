421fda64dc899fb27242a4d232b748c98b659065
Yash Chauhan <Yash.chauhanyrc@gmail.com>
2025-08-07 11:05:23 +0530

    Created several Bug Apis

--- Syncora-Spring-Backend/syncora-project/src/main/java/com/syncora/services/BugService.java
+++ Syncora-Spring-Backend/syncora-project/src/main/java/com/syncora/services/BugService.java
@@ -5,9 +5,16 @@ import java.util.List;
 import com.syncora.dtos.ApiResponse;
 import com.syncora.dtos.BacklogItemDto;
 import com.syncora.dtos.BugReqDto;
+import com.syncora.dtos.BugRespDto;
+import com.syncora.dtos.TaskReqDto;
+import com.syncora.enums.TaskStatus;
 
 public interface BugService {
 
 	ApiResponse createBug(BugReqDto dto, Long id);
 	List<BacklogItemDto> getBacklogBugs();
+	ApiResponse updateBug(BugReqDto dto, Long id);
+	ApiResponse changeStatus(TaskStatus changestatus, Long id);
+	List<BugRespDto> getBugByStatus(Long id);
+	ApiResponse deleteBug(Long id);
 }
