d5bfba60acb1bea08cb7a97dba7b04b8eb9b1812
Priyanshu <priyanshuanand60@gmail.com>
2025-08-07 09:00:02 +0530

    modal added in manager panel

--- Syncora-Spring-Backend/syncora-project/src/main/java/com/syncora/servicesimpl/TaskServiceImpl.java
+++ Syncora-Spring-Backend/syncora-project/src/main/java/com/syncora/servicesimpl/TaskServiceImpl.java
@@ -70,6 +70,11 @@ public ApiResponse createTask(TaskReqDto dto,Long id) {
 				 .orElseThrow(()-> new ResourceNotFoundException("Story Does not exist")) ;
 		createdTask.setStory(story);
 		createdTask.setStatus(TaskStatus.TODO);
+		if(story.getCurrentSprint() != null) {
+			Sprint sprint = sprintRepo.findById(story.getCurrentSprint().getId()).orElseThrow(()->
+			new ResourceNotFoundException("Sprint does not exist by this id")) ;  
+			createdTask.setSprint(sprint);
+		}
 	}
 	
 	taskRepo.save(createdTask) ; 
@@ -110,6 +115,11 @@ public ApiResponse updateTask(TaskReqDto dto, Long id) {
 				 .orElseThrow(()-> new ResourceNotFoundException("Story Does not exist")) ;
 		task.setStory(story);
 		task.setStatus(TaskStatus.TODO);
+		if(story.getCurrentSprint() != null) {
+			Sprint sprint = sprintRepo.findById(story.getCurrentSprint().getId()).orElseThrow(()->
+			new ResourceNotFoundException("Sprint does not exist by this id")) ;  
+			task.setSprint(sprint);
+		}
 	}
 	
     task.setAssignedTo(assignedToEmp);
