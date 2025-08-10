bc5b41e395a7ab97f9acae287005502a188d63c5
Priyanshu <priyanshuanand60@gmail.com>
2025-08-10 13:20:57 +0530

    changes done in App and ProjectDetails.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -29,6 +29,7 @@ import BacklogTable from "./components/BacklogTable/BacklogTable";
 
 import Calendar from "./components/Calendar/Calendar";
 import { ToastContainer } from "react-toastify";
+import ProjectDetails from '../src/components/shared/ProjectDetails'
 
 // import ProjectDetails from "./components/shared/ProjectDetails";
 
@@ -139,7 +140,7 @@ function App() {
         <Route path="/" element={<AboutProject />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/unautho" element={<UnAuthorised />} />
-        {/* <Route path="/project/:projectName" element = {<ProjectDetails/>} /> */}
+       <Route path="/project/:projectName" element = {<ProjectDetails/>} />
       </Routes>
       <ToastContainer
         position="top-right"
