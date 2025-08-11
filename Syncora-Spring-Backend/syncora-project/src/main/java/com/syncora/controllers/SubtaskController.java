0061575c8d8f59be8ce6e74474b8fdcaceb142fa
Yash Chauhan <Yash.chauhanyrc@gmail.com>
2025-08-11 22:36:13 +0530

    Created some new apis and made some changes to the dtos

--- Syncora-Spring-Backend/syncora-project/src/main/java/com/syncora/controllers/SubtaskController.java
+++ Syncora-Spring-Backend/syncora-project/src/main/java/com/syncora/controllers/SubtaskController.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -56,5 +58,9 @@ public class SubtaskController {
 		Long id = jwtUtils.getUserIdFromJwtToken(authHeader);
 		return ResponseEntity.status(HttpStatus.CREATED).body(subtaskService.createSubtask(dto, id));
 	}
-
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteSubtask(@PathVariable Long id) {
+	    return ResponseEntity.status(HttpStatus.CREATED).body(subtaskService.deleteSubtask(id)) ; 
+    }
 }
